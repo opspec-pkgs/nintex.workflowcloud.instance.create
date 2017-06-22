10b8918ed6140667abcfc60929153756edb3fe1e
chris dostert <chris.dostert@nintex.com>
2017-06-21 19:54:35 -0700

    chore) update docs

--- cmd.sh
+++ cmd.sh
@@ -4,12 +4,12 @@ set -e
 
 url="${baseUrl}/api/v1/workflow/published/${workflowId}/instances/create?token=${workflowToken}"
 
-touch /instanceId
+touch /instanceToken
 
-statusCode=$(curl --silent --output /instanceId --write-out "%{http_code}" -X POST "$url")
+statusCode=$(curl --silent --output /instanceToken --write-out "%{http_code}" -X POST "$url")
 
 if test "$statusCode" -ne 201; then
-echo "$(cat /instanceId)"
+echo "$(cat /instanceToken)"
 exit 1
 fi
-echo "instanceId=$(cat /instanceId)"
+echo "instanceToken=$(cat /instanceToken)"
